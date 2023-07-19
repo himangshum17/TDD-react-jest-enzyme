443c4c3054b12d9c46cf34529f999c80ffa1675f
Himangshu Mondal <mailtoherock@gmail.com>
2023-07-19 17:04:02 +0530

    initial enzyme setup and create a basic test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// setup enzyme's react adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+test('should render without crashing', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test-id='component-app']");
+  // assert
+  expect(appComponent.length).toBe(1);
 });
+
+test('should render the increment button', () => {
+  const wrapper = shallow(<App />);
+  const buttonElement = wrapper.find("[data-test-id='button-element']");
+  // assert
+  expect(buttonElement.length).toBe(1);
+});
+
+test('should render the counter display', () => {
+  const wrapper = shallow(<App />);
+  const counterDisplayElement = wrapper.find(
+    "[data-test-id='counter-display-element']"
+  );
+  // assert
+  expect(counterDisplayElement.length).toBe(1);
+});
+
+test('should render the counter display with 0 initially', () => {});
+
+test('should increment the counter display on icrement button clicked', () => {});
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' data-test-id='component-app'>
+      <h1 data-test-id='counter-display-element'>0</h1>
+      <button data-test-id='button-element'>increment button</button>
     </div>
   );
 }
